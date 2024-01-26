1a1a6a9780ca74c43a69e2b02fdadc84222fdf08
Aijun Fan <fanaijun87@sina.com>
2024-01-26 10:58:30 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP TEXT</title>
+</head>
+<body>
+<!-- Hello comments   -->
+<?php
+//Hello comments
+echo "<h1>Hello World 12344</h1>";
+echo "My car is".$color."<br>";
+?>
+
+</body>
+</html>
+
